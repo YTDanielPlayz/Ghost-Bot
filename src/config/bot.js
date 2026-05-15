3ad9fd5cc515b7f4bf8e38bcff91e46c2bb16324
YTDanielPlayz <103783803+YTDanielPlayz@users.noreply.github.com>
2026-05-15 15:02:18 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        // Text users will see (example: "Playing /help | Titan Bot").
+        // Text users will see (example: "Playing /help | Ghost Bot").
         name: "Playing Ghost SkyGen",
         // Activity type number (0 = Playing).
         type: 0, 
